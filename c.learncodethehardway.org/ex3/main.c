abe9a4ac56faf0cacc79dad26b718c85571009e9
GochoMugo <mugo@forfuture.co.ke>
2015-10-13 01:35:13 +0300

    [c.learncodethehardway.org] Add Ex 3

---
+++ c.learncodethehardway.org/ex3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int integer = 7;
+    char character = 'e';
+    float float_num = 3.12;
+    double double_num = 12.32;
+
+    printf("integer:   %d\n", integer);
+    printf("character: %c\n", character);
+    printf("float:     %f\n", float_num);
+    printf("double:    %f\n", double_num);
+
+    return 0;
+}
